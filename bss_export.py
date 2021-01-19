90a1980ee61710d3033a79c9e530bcf9e9197336
ijgr11 <ijgr11@gmail.com>
2021-01-18 22:40:53 -0600

    Initial Commit

---
+++ bss_export.py
@@ -0,0 +1,39 @@
+#!/Users/isman/.pyenv/versions/3.7.7/bin python
+import sys
+from bs4 import BeautifulSoup
+
+try:
+    if len(sys.argv) >= 1:
+        file_name = sys.argv[1]
+        with open(file_name, 'r') as f:
+            soup = BeautifulSoup(f, "lxml")
+
+        link_tag = soup.head.find_all(
+            'link', attrs={'rel': 'stylesheet'}, recursive=False)
+
+        for link in link_tag:
+            link['as'] = 'style'
+            link['onload'] = "this.rel = 'stylesheet'"
+            link['rel'] = 'preload'
+            # Extract all hrefs from link tags
+            hrefs = link.get('href')
+            # Create new Tags: noscript & link
+            noscript_tag = soup.new_tag('noscript')
+            noscript_link_tag = soup.new_tag('link')
+            soup.head.append(noscript_tag)
+            noscript_tag.append(noscript_link_tag)
+            # Add new sub tags to noscripts
+            noscript_link_tag['rel'] = 'stylesheet'
+            noscript_link_tag['href'] = hrefs
+
+        script_tag = soup.find_all('script')
+
+        for script in script_tag:
+            script.attrs['async'] = None
+
+        # Sent create new file
+        with open(file_name, "w") as final_file:
+            final_file.write(soup.prettify())
+            # final_file.write(str(soup))
+except IndexError:
+    print('Please add a file to export')
---
+++ export_settings.sh
@@ -0,0 +1,22 @@
+
+#!/bin/bash
+
+# this will read all html files and replace dj-* attributes with django code
+# YOU MAY NEED TO copy the python export.py script to the same folder as where BSS app image is located
+
+logfile="/tmp/bss_export.log"
+
+# if you have a venv setup, replace the following
+source ~/.pyenv/versions/3.7.7/envs/python_tools/bin/activate
+
+echo "run at `date`" >> $logfile
+echo "`which python3`" >> $logfile
+echo /Users/isman/Documents/devproj/tools >> $logfile
+echo $1 >> $logfile
+pushd $1
+for f in *.html
+do
+    echo python3 /Users/isman/Documents/devproj/tools/bss_export.py $f >> $logfile 
+    python3 /Users/isman/Documents/devproj/tools/bss_export.py $f 
+done
+popd
